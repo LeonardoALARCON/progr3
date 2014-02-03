f73bd64394e83227b098189ee8cf6917d28946a4
miguelmoralesmqz <jorge.miguel.morales.marquez@gmail.com>
2014-02-03 16:43:45 +0100

    Ajouté des tests

--- Persistance/src/fr/univnantes/iut/test/TEST.java
+++ Persistance/src/fr/univnantes/iut/test/TEST.java
@@ -2,13 +2,45 @@ package fr.univnantes.iut.test;
 
 import static org.junit.Assert.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.junit.Test;
+
+import services.AdherantService;
+import entities.Adherant;
 
 public class TEST {
 
 	@Test
 	public void testBDD() {
-		
+		public static void main(String[] args) { 
+			EntityManagerFactory emf =	Persistence.createEntityManagerFactory("td");
+			// Récupération d’une instance de "EntityManager"
+				EntityManager em = emf.createEntityManager(); 
+				// Utilisation de l’ "EntityManager" ...
+				System.out.println("EntityManager open ? "+ em.isOpen());
+				
+				AdherantService as = new AdherantService(emf, em);
+
+				Adherent ad = new Adherent();
+				ad.setId("");
+				ad.setMotPasse("");
+				ad.setnom("");
+				ad.setPrenom("");
+				ad.setAdresse("");
+				ad.setCodePostal("");
+				ad.setVille("");
+				ad.setPays("");
+				
+				as.create(a);
+				
+				as.list(ad.getId());
+								
+				// Fermeture de l’ "EntityManager"
+				em.close(); 
+			emf.close();
 	}
 
 }
