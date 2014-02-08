d886c048ff0a6328d8a25d0bbf1bc4a1b3f394a1
LeCoqGildas <silwold@hotmail.com>
2014-02-08 14:33:10 +0100

    transfert dans une classe de test

--- Persistance/src/fr/univnantes/iut/service/CommandeService.java
+++ Persistance/src/fr/univnantes/iut/service/CommandeService.java
@@ -65,4 +65,5 @@ public class CommandeService {
 		return commandes;
 
 	}
+	
 }
--- Persistance/src/fr/univnantes/iut/test/TestProcedures.java
+++ Persistance/src/fr/univnantes/iut/test/TestProcedures.java
@@ -1,13 +1,42 @@
 package fr.univnantes.iut.test;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 import fr.univnantes.iut.beans.Adherent;
 import fr.univnantes.iut.service.AdherentService;
 
-public class TestBase {
+public class TestProcedures {
 
-	public static void main(String[] args) {
+	@Test
+	public void testCreationAdherent() {
 		AdherentService as = new AdherentService();
+		Adherent ad = new Adherent();
+		ad.setId("coq_sportif38");
+		ad.setMotPase("im_a_princess");
+		ad.setNom("GILDAS");
+		ad.setPrenom("LE COQ");
+		ad.setAdresse("15, PONT SOUR L'ERDRE");
+		ad.setCodePostal("44000");
+		ad.setVille("NANTES");
+		ad.setPays("FRANCE");
+		as.create(ad);
+		
+	}
+	
+	@Test
+	public void testSuppretionAdherent() {
+		AdherentService as = new AdherentService();
+		
+		as.delete("coq_sportif38");
+	}
+	
+	@Test
+	public void testAffichageAdherents() {
+		AdherentService as = new AdherentService();
+		as.delete("coq_sportif38");
+		as.delete("LeoNardo");
 		
 		Adherent ad = new Adherent();
 		ad.setId("coq_sportif38");
@@ -29,7 +58,6 @@ public class TestBase {
 		ad1.setCodePostal("44000");
 		ad1.setVille("NANTES");
 		ad1.setPays("FRANCE");
-		as.create(ad);
 		as.create(ad1);
 		
 		System.out.println("ok");	
@@ -37,10 +65,9 @@ public class TestBase {
 			
 			System.out.println(a.toString());
 		}
-		
 		as.delete("coq_sportif38");
 		as.delete("LeoNardo");
 
 	}
-
+	
 }
